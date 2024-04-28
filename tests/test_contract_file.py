1d4ce9dbae40ad375637ac3add79e66020de2ee3
Mikhail Vasiliev <m89154280096@yandex.ru>
2024-04-28 03:57:11 +0300

    file attach fixed

--- tests/test_contract_file.py
+++ tests/test_contract_file.py
@@ -5,6 +5,6 @@ from biblio_globus_models.models.models import Profile
 @allure.story('Check contract file')
 def test_contract_file():
     page = Profile()
-    #page.open('')
-    #page.login()
-    doc = page.download_contract_template()
+    page.open('')
+    page.login()
+    page.download_contract_template()
